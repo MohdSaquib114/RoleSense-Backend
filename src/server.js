6a90a62fba1e57acff6686c15c3e363c22b75de8
MohdSaquib114 <msmsaq114@gmail.com>
2025-05-30 22:07:35 +0530

    merger http and worker

--- src/server.js
+++ src/server.js
@@ -1,8 +1,17 @@
-const app = require("./app");
+// server.js
+
 require("dotenv").config();
+const app = require("./app");
+const rolePredictionQueue = require('./queue/rolePredictionQueue');
+const rolePredictionProcess = require('./queue/job/rolePredictionProcess');
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
+
+
+rolePredictionQueue.process(rolePredictionProcess);
+
+console.log('👷 Worker started and listening for role prediction jobs...');
